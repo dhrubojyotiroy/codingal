e4f2df89ef31fb0b0b792d7f5baad1def9b9559f
dhrubojyotiroy <dhrubojyotiroy100@gmail.com>
2024-09-04 14:55:39 +0600

    Update Add More Sprites.py

--- Add More Sprites.py
+++ Add More Sprites.py
@@ -1,1 +1,10 @@
+class myobject:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def __index__(self):
+        return self.x + self.y
 
+
+obj = myobject(8,4)
+print(bin(obj))
